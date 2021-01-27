573fbbbb05edcb6150f4c6653a0a2a48a566638a
jseg787 <jsegarra3@gmail.com>
2021-01-27 12:50:06 -0500

    add temp album artwork, add styling for mobile

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const body = document.querySelector('body');
+const library = document.querySelector('.library');
 
 let myLibrary = [];
 
@@ -16,17 +16,71 @@ addAlbumToLibrary('Chon', 'Chon', 2019);
 addAlbumToLibrary('Homey', 'Chon', 2017);
 addAlbumToLibrary('Grow', 'Chon', 2015);
 addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
+addAlbumToLibrary('Chon', 'Chon', 2019);
+addAlbumToLibrary('Homey', 'Chon', 2017);
+addAlbumToLibrary('Grow', 'Chon', 2015);
+addAlbumToLibrary('Newborn Sun', 'Chon', 2013);
 
 myLibrary.forEach((album) => {
 	const card = document.createElement('div');
 	const title = document.createElement('p');
-	title.innerText = album.title;
 	const artist = document.createElement('p');
-	artist.innerText = album.artist;
 	const year = document.createElement('p');
+	const image = document.createElement('img');
+	const imageContainer = document.createElement('div');
+
+	card.classList.add('album');
+	imageContainer.classList.add('artwork');
+
+	title.innerText = album.title;
+	artist.innerText = album.artist;
+	image.src = './temp.jpg';
 	year.innerText = album.year;
+
+	imageContainer.appendChild(image);
+	card.appendChild(imageContainer);
 	card.appendChild(title);
 	card.appendChild(artist);
 	card.appendChild(year);
-	body.appendChild(card);
+	library.appendChild(card);
 });
